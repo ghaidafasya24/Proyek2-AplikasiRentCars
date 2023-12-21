cfb8aeef651679cc9ea53fda17715502e341029c
Ichika354 <handa82624@gmail.com>
2023-12-21 23:00:00 +0700

    katalog

--- resources/views/Customer/katalogMobil.blade.php
+++ resources/views/Customer/katalogMobil.blade.php
@@ -9,7 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="../../assets/css/dashboard.css" rel="stylesheet" />
     <link rel="stylesheet" href="../../assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 
 </head>
@@ -33,10 +34,11 @@
                         </div>
                         <div class="col-md-8">
                             <form action="" method="post">
-                                <div class="card-body">
-                                    <input type="hidden" name="id" value="">
-                                    <h5 class="card-title">Pajero Sport</h5>
-                                    {{-- <h6 class="card-text">Tahun :</h6>
+                                @foreach ($mobils as $mobil)
+                                    <div class="card-body">
+                                        <input type="hidden" name="id" value="">
+                                        <h5 class="card-title">{{ $mobil->merek_mobil }}</h5>
+                                        {{-- <h6 class="card-text">Tahun :</h6>
                                     <p> 2022 </p>
                                     <input type="hidden" name="ketua" value="">
                                     <h6 class="card-text"> Warna :</h6>
@@ -45,11 +47,14 @@
                                     <h6 class="card-text"> Kapasitas Penumpang :</h6>
                                     <p> 4 </p>
                                     <input type="hidden" name="ketua" value=""> --}}
-                                    <h6 class="card-text"> Harga Sewa :</h6>
-                                    <p> Rp. 1.500.000</p>
-                                    <input type="hidden" name="wakil" value="">
-                                    <button href="{{ route('detailMobil') }}" name="" class="btn btn-primary">Detail</button>
-                                </div>
+                                        <h6 class="card-text"> Harga Sewa :</h6>
+                                        <p> Rp. {{ number_format($mobil->harga, 0, ',', '.') }}</p>
+                                        <input type="hidden" name="wakil" value="">
+                                        <button href="{{ route('detailMobil') }}" name=""
+                                            class="btn btn-primary">Detail</button>
+                                    </div>
+                                @endforeach
+
                             </form>
                         </div>
                     </div>
@@ -59,7 +64,9 @@
     </div>
     <!-- content end  -->
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
     <script src="js/scripts.js"></script>
