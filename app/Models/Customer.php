9fe482e5e9c2c65e56af3ac33bcf7827a6b77af6
Ichika354 <handa82624@gmail.com>
2023-12-23 16:38:22 +0700

    'Model'

---
+++ app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_customer';
+
+    protected $fillable = [
+        'nama_customer',
+        'no_telp',
+        'ktp',
+        'email',
+        'alamat',
+        'nama_orang_terdekat',
+        'email_darurat',
+        'no_telp',
+    ];
+}
