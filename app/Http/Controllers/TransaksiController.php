a8721d5a591bd60a271a519ad7c2bc8d7fb5d9fd
ghaidafasya24 <ghaidafasya5@gmail.com>
2023-12-23 16:19:01 +0700

    revisi frontend form pembayaran

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class RiwayatTransaksiController extends Controller
+class TransaksiController extends Controller
 {
     public function riwayattransaksi(){
         return view('Admin.RiwayatTransaksi.riwayattransaksi');
+    }
+
+    public function transaksiPembayaran(){
+        return view('Customer.transaksiPembayaran');
     }
 }
