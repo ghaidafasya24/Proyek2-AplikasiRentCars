6afbbb15a393ac606187b21e63fd199c721bee73
ghaidafasya24 <ghaidafasya5@gmail.com>
2023-12-24 11:33:31 +0700

    struk pembayaran

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 
 class TransaksiController extends Controller
 {
-    public function riwayattransaksi(){
+    public function riwayattransaksi()
+    {
         return view('Admin.RiwayatTransaksi.riwayattransaksi');
     }
 
-    public function transaksi(Request $request){
+    public function transaksi(Request $request)
+    {
         return view('Customer.transaksiPembayaran');
     }
+
+    
 }
